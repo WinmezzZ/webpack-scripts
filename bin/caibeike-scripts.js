674097c70ccb53db7e98f414956d67bf15ae6de6
Winme <winme0308@gmail.com>
2022-03-28 11:42:12 +0800

    fix: execa => crossSpawn

--- bin/caibeike-scripts.js
+++ bin/caibeike-scripts.js
@@ -6,7 +6,7 @@ process.on('unhandledRejection', err => {
     throw err;
 });
 
-const execa = require('execa');
+const spawn = require('react-dev-utils/crossSpawn');
 const args = process.argv.slice(2);
 
 const scriptIndex = args.findIndex(x => x === 'build' || x === 'start');
@@ -14,8 +14,8 @@ const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
 const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
 
 if (['build', 'start'].includes(script)) {
-    const result = execa.sync(
-        'node',
+    const result = spawn.sync(
+        process.execPath,
         nodeArgs.concat(require.resolve('../scripts/' + script)).concat(args.slice(scriptIndex + 1)),
         { stdio: 'inherit' },
     );
